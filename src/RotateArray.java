9f6fda257c3e4422846bbbf155e220448bd13f27
haridevadharshini <haridevadharshini311@gmail.com>
2026-03-05 22:08:49 +0530

    Your commit message

--- src/RotateArray.java
+++ src/RotateArray.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70};
+        int k = 2;
+        int n = arr.length;
+        int[] res = new int[n];
+        for (int i = 0; i <= k; i++) {
+            res[k - i] = arr[i];
+        }
+        for (int i = k + 1; i < n; i++) {
+            res[n - 1 - (i - (k + 1))] = arr[i];
+        }
+        System.out.println(Arrays.toString(res));
+    }
 }
